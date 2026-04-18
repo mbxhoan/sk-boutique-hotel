c3f09a1765fd8ca7818953be4d71b8e0eec65da6
Mai Bui Xuan Hoan <mbxhoan001@gmail.com>
2026-04-18 17:07:40 +0700

    logo no bg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.2.109"],
+  allowedDevOrigins: [
+    "localhost", 
+    "127.0.0.1", 
+    "192.168.2.109",
+    "192.168.2.43"
+  ],
   images: {
     qualities: [85]
   }
